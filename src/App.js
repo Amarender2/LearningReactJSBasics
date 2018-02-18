907a240d9ebb913367f2f4551ab7235fae0c6d38
amarender.mekala <amarender.mekala@gmail.com>
2018-02-17 20:34:16 -0600

    Lists and Conditionals

--- src/App.js
+++ src/App.js
@@ -3,6 +3,75 @@ import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
+// class App extends Component {
+//   // Not available in function components.
+//   // Manipulate state in less places, so app can be maintained well
+//   // So use function components if possible
+//   // If state changes, dom is re-rendered by react 
+//   state = {
+//     persons: [
+//       { name: 'Amar', age: 26 },
+//       { name: 'Reddy', age: 28 },
+//       { name: 'Mekala', age: 30 },
+//     ]
+//   }
+
+//   switchNameHandler = (newName) => {
+//     console.log('Function Called');
+//     // Dont do this: this.state.persons.name[0]='pranit';
+//     this.setState({
+//       persons: [
+//       { name: newName, age: 26 },
+//       { name: 'Reddy123', age: 28 },
+//       { name: 'Mekala', age: 32 },
+//       ]
+//     });
+//   }
+
+//   // event will be passed by react
+//   nameChangedHandler = (event) => {
+//     this.setState({
+//       persons: [
+//       { name: 'Amarender', age: 26 },
+//       { name: event.target.value, age: 28 },
+//       { name: 'Mekala', age: 32 },
+//       ]
+//     });
+//   }
+
+//   render() {
+
+//     const style = {
+//       backgroundColor : 'white',
+//       font : 'inherit',
+//       border : '1px solid blue',
+//       padding : '8px',
+//       cursor : 'pointer',
+//     }
+//     return (
+//       // class cannot be used 
+//       // One root element
+//       // react provides div, h1 (JSX not HTML)
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <h1>Hi, I am learning React</h1>
+//         <p>Hi, I am learning React 2</p>
+//         {/* Dont add parantheses, just pass referencem or else function gets executed when diplayed*/}
+//         {/* This can be ineffcient, react can re-render too often if this way of handler is used, use bind when possible */}
+//         <button style={style} onClick={(event) => this.switchNameHandler('Amarender$')}>Switch Name</button>
+//         <Person click={this.switchNameHandler.bind(this, 'Pranit')} name={this.state.persons[0].name} age={this.state.persons[0].age} />
+//         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}> Component Children </Person>
+//         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+//       </div>
+//     );
+//     // compiles to this
+//     // return React.createElement('div', {className : 'App'}, React.createElement('h1', null, React.createElement('h1', null, 'Hi, I am good')), React.createElement('h2', null, 'Hi, I am good2'));
+//   }
+// }
+
 class App extends Component {
   // Not available in function components.
   // Manipulate state in less places, so app can be maintained well
@@ -10,43 +79,71 @@ class App extends Component {
   // If state changes, dom is re-rendered by react 
   state = {
     persons: [
-      { name: 'Amar', age: 26 },
-      { name: 'Reddy', age: 28 },
-      { name: 'Mekala', age: 30 },
-    ]
+      {id: '234rwrwe',  name: 'Amar', age: 26 },
+      {id: 'hjhj',  name: 'Reddy', age: 28 },
+      {id: 'asdasd',  name: 'Mekala', age: 30 },
+    ],
+    showPersons: false,
   }
 
-  switchNameHandler = (newName) => {
-    console.log('Function Called');
-    // Dont do this: this.state.persons.name[0]='pranit';
+  deletePersonHandler = (index) => {
+    //let exisitingPersons = this.state.persons.slice();
+    let exisitingPersons = [...this.state.persons];
+    exisitingPersons.splice(index, 1);
+
     this.setState({
-      persons: [
-      { name: newName, age: 26 },
-      { name: 'Reddy123', age: 28 },
-      { name: 'Mekala', age: 32 },
-      ]
+      persons: exisitingPersons
     });
   }
 
-  // event will be passed by react
-  nameChangedHandler = (event) => {
+  togglePersons = () => {
     this.setState({
-      persons: [
-      { name: 'Amarender', age: 26 },
-      { name: event.target.value, age: 28 },
-      { name: 'Mekala', age: 32 },
-      ]
+      showPersons: !this.state.showPersons
+    })
+  }
+
+  // event will be passed by react
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id); // find
+
+    const per = {...this.state.persons[personIndex]};
+    // const per = Object.assign({}, person); copies object properties into other, makes another copy
+    per.name = event.target.value;
+
+    const persons = this.state.persons.slice(); // or use spread operator ... 
+    persons[personIndex] = per;
+
+    this.setState({
+      persons: persons
     });
   }
 
   render() {
 
     const style = {
-      backgroundColor : 'white',
-      font : 'inherit',
-      border : '1px solid blue',
-      padding : '8px',
-      cursor : 'pointer',
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    }
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (<div>
+        {
+          this.state.persons.map((person, index) => {
+            return <Person name={person.name} age={person.age} 
+                      click={() => this.deletePersonHandler(index)}
+                      key={person.id}
+                      changed={(event) => (this.nameChangedHandler(event, person.id))}/> 
+                      // or use index as key, this is to help react update dom easily
+          })
+        }
+      </div>)
+      // <Person click={this.switchNameHandler.bind(this, 'Pranit')} name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      // <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}> Component Children </Person>
+      // <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
     }
     return (
       // class cannot be used 
@@ -61,10 +158,13 @@ class App extends Component {
         <p>Hi, I am learning React 2</p>
         {/* Dont add parantheses, just pass referencem or else function gets executed when diplayed*/}
         {/* This can be ineffcient, react can re-render too often if this way of handler is used, use bind when possible */}
-        <button style={style} onClick={(event) => this.switchNameHandler('Amarender$')}>Switch Name</button>
-        <Person click={this.switchNameHandler.bind(this, 'Pranit')} name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}> Component Children </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button style={style} onClick={this.togglePersons}>Toggle Names</button>
+        {persons}
+        {/* {
+          this.state.showPersons ?  */}
+
+        {/* : null
+        } */}
       </div>
     );
     // compiles to this
